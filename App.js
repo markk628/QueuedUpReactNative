b3ea8072228d119dbde0d025e9957b534f2c1202
Donghyun <markk628@gmail.com>
2020-06-15 11:23:36 -0700

    need api key

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
@@ -17,3 +18,64 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const SongQueue = styled.View
+  'margin-top: 20px;';
+
+const PAGE = 20;
+
+class QueuedUp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      songs: [],
+      offset: 0,
+      query: "",
+      isFetching: false,
+      isEmpty: false,
+      spotify_tokens: null,
+      isTokenFetching: false,
+    };
+  }
+
+  handleSearchChange(text) {
+    this.setState(
+      {
+        isEmpty: false,
+        query: text,
+        offset: 0,
+        songs: []
+      },
+      () => {
+        this.loadNextPage();
+      }
+    );
+  }
+
+  render() {
+    const{ query } = this.state;
+
+    return (
+      <Container>
+        <SafeAreaView>
+          <Header>
+            <title>
+              "Music"
+            </title>
+          </Header>
+          <SearchBar>
+            onChange = { text => this.handleSearchCange(text) }
+            text = { query }
+          </SearchBar>
+          <SongQueue>
+
+          </SongQueue>
+        </SafeAreaView>
+      </Container>
+    );
+  }
+}
+
+export default QueuedUp;
+
